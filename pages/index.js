b58a5dd73981d49fcb97747c84a1b51ed1890601
koshef <mykosherchef.food@gmail.com>
2025-06-09 15:56:36 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
+import { useState } from 'react';
 import ImageUploader from '../components/ImageUploader';
 
 export default function Home() {
+  const [imageUrl, setImageUrl] = useState('');
+
   const handleImageUploaded = (url) => {
+    setImageUrl(url);
     console.log('Image uploaded to:', url);
-    // You can later pass this to a recipe form
   };
 
   return (
     <main>
       <h1>Upload a Recipe Image</h1>
       <ImageUploader onUploaded={handleImageUploaded} />
+      {imageUrl && (
+        <div>
+          <p>Uploaded Image:</p>
+          <img src={imageUrl} alt="Uploaded Recipe" style={{ maxWidth: '300px' }} />
+        </div>
+      )}
     </main>
   );
 }
