beab31f96709f19eaf2880f5ee994f6a7ac785b9
koshef <mykosherchef.food@gmail.com>
2025-06-10 23:04:26 -0400

    Update next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: false, // Needed if you're using the /pages directory with file uploads
+  },
 };
 
 module.exports = nextConfig;
