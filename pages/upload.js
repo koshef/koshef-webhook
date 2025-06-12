dc0fd4342874606ec6c8f5ab15d43acc4ba9979a
koshef <mykosherchef.food@gmail.com>
2025-06-12 14:27:37 -0400

    Create upload.js

---
+++ pages/upload.js
@@ -0,0 +1,10 @@
+import ImageUploader from '../components/ImageUploader';
+
+export default function UploadPage() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1 style={{ color: '#C1694F' }}>Upload a Recipe Image</h1>
+      <ImageUploader />
+    </main>
+  );
+}
